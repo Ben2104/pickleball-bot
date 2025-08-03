eb06858d6870882fe0b6097151da578fd285002e
Ben2104 <dohoangkhoi341@gmail.com>
2025-08-02 21:45:43 -0700

    change timeslots from 7-9pm

--- reserve.js
+++ reserve.js
@@ -23,8 +23,8 @@ const BOOKING_URL = '/book/ipicklecerritos';
 const COURT_TYPE = 'Pickleball';
 const TIME_SLOTS = ["7-7:30pm", "7:30-8pm", "8-8:30pm", "8:30-9pm"];
 
-const BOOKING_HOUR = parseInt(process.env.BOOKING_HOUR) || 12;
-const BOOKING_MINUTE = parseInt(process.env.BOOKING_MINUTE) || 46;
+const BOOKING_HOUR = parseInt(process.env.BOOKING_HOUR) || 7;
+const BOOKING_MINUTE = parseInt(process.env.BOOKING_MINUTE) || 0;
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 // Stealth configuration
