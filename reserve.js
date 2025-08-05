96b8226f41cc68c4caea8b102f2e3734915abfb3
Ben2104 <dohoangkhoi341@gmail.com>
2025-08-05 15:20:55 -0700

    update book.yml and reverse.js

--- reserve.js
+++ reserve.js
@@ -28,7 +28,7 @@ let TIME_SLOTS;
 let courtPriorityMap;
 
 if (USER_NAME === 'Khoi Do') {
-    TIME_SLOTS = ["7-7:30am", "7:30-8am", "8-8:30am", "8:30-9am"];
+    TIME_SLOTS = ["8-8:30pm", "8:30-9pm", "9-9:30pm", "9:30-10pm"];
     courtPriorityMap = new Map([
         [0, "PICKLEBALL 2"],
         [1, "PICKLEBALL 4"],
