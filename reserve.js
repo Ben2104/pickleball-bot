93c7f2bfa07223c79add0168e9f23e35b678bcf5
Ben2104 <dohoangkhoi341@gmail.com>
2025-08-05 15:37:05 -0700

    update reverse.js

--- reserve.js
+++ reserve.js
@@ -28,7 +28,7 @@ let TIME_SLOTS;
 let courtPriorityMap;
 
 if (USER_NAME === 'Khoi Do') {
-    TIME_SLOTS = ["7-7:30am", "7:30-8am", "8-8:30am", "8:30-9am"];
+    TIME_SLOTS = ["8-8:30pm", "8:30-9pm", "9-9:30pm", "9:30-10pm"];
     courtPriorityMap = new Map([
         [0, "PICKLEBALL 2"],
         [1, "PICKLEBALL 4"],
@@ -157,7 +157,7 @@ export async function addCalendarEvent(startDateTime, endDateTime) {
         console.log('End time:', endDateTime);
 
         const event = {
-            summary: '🏓 Pickleball Reservation',
+            summary: `🏓 ${USER_NAME}'s court,  court: ${selectedCourt}`,
             location: 'iPickle Cerritos',
             description: `${USER_NAME}'s court,  court: ${selectedCourt}`,
             start: {
