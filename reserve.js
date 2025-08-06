08b8b28700d7caf88e0d6d71f1f46d04503f2f15
Ben2104 <dohoangkhoi341@gmail.com>
2025-08-05 22:50:55 -0700

    update new job for Marvin

--- reserve.js
+++ reserve.js
@@ -43,7 +43,7 @@ if (USER_NAME === 'Khoi Do') {
     ]);
 }
 else if (USER_NAME === 'Marvin') {
-    TIME_SLOTS = ["7:30-8pm", "8-8:30pm", "8:30-9pm", "9-9:30pm"];
+    TIME_SLOTS = ["7-7:30am", "7:30-8am", "8-8:30am", "8:30-9am"];
     courtPriorityMap = new Map([
         [0, "PICKLEBALL 5"],
         [1, "PICKLEBALL 10"],
