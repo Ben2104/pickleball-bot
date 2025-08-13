65d89de5f6d1eb3356577eb66bbebde7e22d50dc
Ben2104 <dohoangkhoi341@gmail.com>
2025-08-13 12:42:55 -0500

    update reserve.js by removing goto calendar link

--- reserve.js
+++ reserve.js
@@ -222,7 +222,7 @@ export async function addCalendarEvent(startDateTime, endDateTime) {
 
         console.log('✅ Event added to calendar');
         console.log('📅 Event link:', response.data.htmlLink);
-        await page.goto(response.data.htmlLink)
+        
         return response.data;
 
     } catch (error) {
